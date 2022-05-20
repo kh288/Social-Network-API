1022f8170b0fe94374ad761e5ec086ed85cf804a
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-20 07:48:34 -0700

    Added temporary test html

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.render('text.html');
+})
+
 app.listen(PORT, () => {
   console.log(`Running on PORT: ${PORT}`);
-})
+});
