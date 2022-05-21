d6b4a47d18680c5cfb85b29945086d0c896d3575
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-20 21:40:20 -0700

    Added additional settings for server.js

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const activity = cwd.includes('01-Activities')
+  ? cwd.split('/01-Activities/')[1]
+  : cwd;
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  try {
-    res.json('Successful Request');
-    res.status(200);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-})
+// app.get('/', (req, res) => {
+//   try {
+//     res.json('Successful Request');
+//     res.status(200);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-app.listen(PORT, () => {
-  console.log(`Running on PORT: ${PORT}`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity}`);
+    console.log(`Running on PORT: ${PORT}`);
+  });
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Running on PORT: ${PORT}`);
+// });
