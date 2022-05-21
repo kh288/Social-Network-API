b5932c5becf21a85f05935f59b46fbb42d22b3b2
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-20 21:42:22 -0700

    Removed random comments

--- server.js
+++ server.js
@@ -14,22 +14,9 @@ const activity = cwd.includes('01-Activities')
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   try {
-//     res.json('Successful Request');
-//     res.status(200);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for ${activity}`);
     console.log(`Running on PORT: ${PORT}`);
   });
 });
-
-// app.listen(PORT, () => {
-//   console.log(`Running on PORT: ${PORT}`);
-// });
