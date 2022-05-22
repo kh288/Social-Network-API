306637016b6e502df87979dece0032057ff97ec0
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-22 14:06:11 -0700

    Created index for model

--- utils/seed.js
+++ utils/seed.js
@@ -7,16 +7,20 @@ connection.on('error', (err) => err);
 connection.once('open', async () => {
   console.log('Connected');
 
-  await User.deleteMany({});
-  await Thought.deleteMany({});
+  // await User.deleteMany({});
+  // await Thought.deleteMany({});
 
   const users = [];
   const thoughts =[];
 
-  // Create 20 users
-  for (let i = 0; i < 20; i++) {
+  // Create i amount of users
+  for (let i = 0; i < 5; i++) {
     users.push(generateUser());
     thoughts.push(generateThought());
   }
-  
+
+  // console.log(users);
+  // console.log(thoughts);
+
+  await User.collection.insertMany(users);
 });
