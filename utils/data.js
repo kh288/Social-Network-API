c23d59fcc56d7b7f23ec11cc8478b5e646bbec59
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-22 11:25:29 -0700

    Added an email and username generation

--- utils/data.js
+++ utils/data.js
@@ -36,3 +36,32 @@ const usernames = [
 'Softpany',
 'Attabra'
 ];
+
+const emails = [
+'gmail',
+'yahoo',
+'hotmail',
+'aol',
+'msn',
+'live',
+'outlook',
+'icloud'
+];
+
+// Generates an email based on their username
+function genEmailFromUsername(username) {
+  const emailPrefix = username.toLowerCase();
+  const emailSuffix = emails[Math.floor(Math.random() * emails.length)];
+  return `${emailPrefix}@${emailSuffix}.com`;
+}
+
+// Generates a user that randomly returns an array username + email
+function generateUser() {
+  const user = [usernames[Math.floor(Math.random() * usernames.length)]];
+  user.push(genEmailFromUsername(user[0]));
+  return user;
+}
+
+// console.log(generateUser());
+
+module.exports = { generateUser };
