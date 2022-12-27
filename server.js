571a45be55eda3ad3c777dba1c22bb8774806cee
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-12-27 11:25:43 -0800

    Cleaned up server.js and connection.js

--- server.js
+++ server.js
@@ -2,22 +2,15 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity}`);
     console.log(`Running on PORT: ${PORT}`);
   });
 });
